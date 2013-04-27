afdcb682b809e5c13755ce40ad825e05219c8c58
Thiago Pio <alemdolimite@gmail.com>
2013-04-26 22:38:51 -0300

    adicionando teste.php com carrossel

---
+++ teste.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>A CR Serviços de limpeza oferece serviços de remoção de sujeira.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Business Target">
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+  	<meta name="title" content="CR Serviços e Limpeza é especialista em limpeza de" />
+  	<meta name="url" content="http://crlimpezas.com.br" />
+  	<meta name="description" content="É especialista em limpeza de interior de carros, persianas, painéis, divisórias, lavagem de sofá, e remoção de sujeira." />
+  	<meta name="keywords" content="lavagem de sofa, lavagem de cadeiras, lavagem de persianas, lavagem de interior de carro" />
+  	<meta http-equiv="Content-Language" content="pt"/>
+  	<link rev="made" href="mailto:comercial@crlimpezas.com.br" />
+
+    <!-- Le styles -->
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="bootstrap-responsive.css" rel="stylesheet">
+    <style>
+
+    /* GLOBAL STYLES
+    -------------------------------------------------- */
+    /* Padding below the footer and lighter body text */
+
+    body {
+      padding-bottom: 40px;
+      color: #5a5a5a;
+    }
+
+    /* CUSTOMIZE THE NAVBAR
+    -------------------------------------------------- */
+
+    /* Special class on .container surrounding .navbar, used for positioning it into place. */
+    .navbar-wrapper {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 10;
+      margin-top: 20px;
+      margin-bottom: -90px; /* Negative margin to pull up carousel. 90px is roughly margins and height of navbar. */
+    }
+    .navbar-wrapper .navbar {
+
+    }
+
+    /* Remove border and change up box shadow for more contrast */
+    .navbar .navbar-inner {
+      border: 0;
+      -webkit-box-shadow: 0 2px 10px rgba(0,0,0,.25);
+         -moz-box-shadow: 0 2px 10px rgba(0,0,0,.25);
+              box-shadow: 0 2px 10px rgba(0,0,0,.25);
+    }
+
+    /* Downsize the brand/project name a bit */
+    .navbar .brand {
+      padding: 14px 20px 16px; /* Increase vertical padding to match navbar links */
+      font-size: 16px;
+      font-weight: bold;
+      text-shadow: 0 -1px 0 rgba(0,0,0,.5);
+    }
+
+    /* Navbar links: increase padding for taller navbar */
+    .navbar .nav > li > a {
+      padding: 15px 20px;
+    }
+
+    /* Offset the responsive button for proper vertical alignment */
+    .navbar .btn-navbar {
+      margin-top: 10px;
+    }
+
+    /* MARKETING CONTENT
+    -------------------------------------------------- */
+
+    /* Center align the text within the three columns below the carousel */
+    .marketing .span4 {
+      text-align: center;
+    }
+    .marketing h2 {
+      font-weight: normal;
+    }
+    .marketing .span4 p {
+      margin-left: 10px;
+      margin-right: 10px;
+    }
+
+
+    /* Featurettes
+    ------------------------- */
+
+    .featurette-divider {
+      margin: 50px 0; /* Space out the Bootstrap <hr> more */
+    }
+    .featurette {
+      overflow: hidden; /* Vertically center images part 2: clear their floats. */
+    }
+
+    .featurette-image {
+      width: 275px;
+    }
+
+    /* Give some space on the sides of the floated elements so text doesn't run right into it. */
+    .featurette-image.pull-left {
+      margin-right: 40px;
+    }
+    .featurette-image.pull-right {
+      margin-left: 40px;
+    }
+
+    /* Thin out the marketing headings */
+    .featurette-heading {
+      font-size: 50px;
+      font-weight: 300;
+      line-height: 1;
+      letter-spacing: -1px;
+    }
+
+
+
+    /* RESPONSIVE CSS
+    -------------------------------------------------- */
+
+    @media (max-width: 979px) {
+
+      .container.navbar-wrapper {
+        margin-bottom: 0;
+        width: auto;
+      }
+      .navbar-inner {
+        border-radius: 0;
+        margin: -20px 0;
+      }
+
+      .featurette {
+        height: auto;
+        padding: 0;
+      }
+      .featurette-image.pull-left,
+      .featurette-image.pull-right {
+        display: block;
+        float: none;
+        max-width: 40%;
+        margin: 0 auto 20px;
+      }
+    }
+
+
+    @media (max-width: 767px) {
+
+      .navbar-inner {
+        margin: -20px;
+      }
+
+      .marketing .span4 + .span4 {
+        margin-top: 40px;
+      }
+
+      .featurette-heading {
+        font-size: 30px;
+      }
+      .featurette .lead {
+        font-size: 18px;
+        line-height: 1.5;
+      }
+
+    }
+    </style>
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="favicon.ico">
+    <link rel="apple-touch-icon-precomposed" href="favicon.ico">
+    <link rel="shortcut icon" href="favicon.ico">
+  </head>
+
+  <body>
+
+    <!-- NAVBAR
+    ================================================== -->
+    <div class="navbar-wrapper">
+      <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
+      <div class="container">
+
+        <div class="navbar navbar-inverse">
+          <div class="navbar-inner">
+            <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="index.php" onClick="_gaq.push(['_trackEvent', 'Clique no menu', 'Serviços', 'Logo']);">CR Serviços de Limpeza</a>
+            <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
+            <div class="nav-collapse collapse">
+              <ul class="nav">
+                <li><a href="index.php" onClick="_gaq.push(['_trackEvent', 'Clique no menu', 'Serviços', 'Home']);">Home</a></li>
+                <li><a href="contato.php">Orçamento</a></li>
+                <li class="active"><a href="servicos.php" onClick="_gaq.push(['_trackEvent', 'Clique no menu', 'Serviços', 'Servicos']);">Serviços</a></li>
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div><!-- /.navbar-inner -->
+        </div><!-- /.navbar -->
+
+      </div> <!-- /.container -->
+    </div><!-- /.navbar-wrapper -->
+
+    <div class="container marketing">
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+        <div id="myCarousel" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="item">
+              <img src="http://placehold.it/1200x500" alt="">
+              <div class="carousel-caption">
+                <h4>First Thumbnail label</h4>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+            </div>
+            <div class="item">
+              <img src="http://placehold.it/1300x510" alt="">
+              <div class="carousel-caption">
+                <h4>Second Thumbnail label</h4>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+            </div>
+            <div class="item active">
+              <img src="http://placehold.it/1400x520" alt="">
+              <div class="carousel-caption">
+                <h4>Third Thumbnail label</h4>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+            </div>
+          </div>
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+        </div>
+
+      <hr class="featurette-divider">
+
+      <div class="featurette">
+        <img class="featurette-image pull-left" src="img/servicos_divisoria.png" alt="limpeza de divisórias">
+        <h2>Limpeza de divisórias</h2>
+        <p>Na limpeza das divisórias utilizamos a aplicação de uma solução sobre a superfície dos módulos, desprendendo a sujidade. Em seguida, a limpeza é feita com uma máquina extratora que injeta uma solução apropriada de limpeza.</p>
+        <p>Devido ao controle de umidade, o período de secagem é quase imediato.</p>
+        <p>Solicite seu <a href="contato.php" onClick="_gaq.push(['_trackEvent', 'Box de seviço', 'Serviços', 'limpeza de divisória']);">orçamento agora</a>.</p>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-left"><a href="contato.php" onclick="_gaq.push(['_trackEvent', 'Rodapé', 'Serviços', 'Orçamento']);">Faça seu orçamento através de nosso site ou ligue</a> (21) 3041-1094 | (21) 8727-1365</p>
+        
+        <p class="pull-right">
+          <a href="http://www.orkut.com.br/Main#Community?cmm=123581865" target="_blank" onclick="_gaq.push(['_trackEvent', 'Rodapé', 'Serviços', 'Orkut']);"><img src="img/ico_orkut.png" alt="Facebook CR Serviços de Limpeza"></a>
+          &middot; 
+          <a href="https://www.facebook.com/CrServicosDeLimpeza" target="_blank" onclick="_gaq.push(['_trackEvent', 'Rodapé', 'Serviços', 'Facebook']);">
+          <img src="img/icon_facebook.png" alt="Orkut CR Serviços de Limpeza"></a>
+        </p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script type="text/javascript">
+      $('.carousel').carousel({
+        interval: 2000
+      })
+  	  var _gaq = _gaq || [];
+  	  _gaq.push(['_setAccount', 'UA-36923842-1']);
+  	  _gaq.push(['_setDomainName', 'crlimpezas.com.br']);
+  	  _gaq.push(['_trackPageview']);
+
+  	  (function() {
+  	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  	  })();
+  	</script>
+  </body>
+</html>
