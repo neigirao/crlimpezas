0326a5c48f1842f9e5c6a4045cef5597358ce749
Thiago Pio <alemdolimite@gmail.com>
2013-04-26 23:01:58 -0300

    adicionando carrossel na lagding page

--- lavagem_sofa.php
+++ lavagem_sofa.php
@@ -16,246 +16,7 @@
     <!-- Le styles -->
     <link href="bootstrap.css" rel="stylesheet">
     <link href="bootstrap-responsive.css" rel="stylesheet">
-    <style>
-
-    /* GLOBAL STYLES
-    -------------------------------------------------- */
-    /* Padding below the footer and lighter body text */
-
-    body {
-      padding-bottom: 40px;
-      color: #5a5a5a;
-    }
-
-
-
-    /* CUSTOMIZE THE NAVBAR
-    -------------------------------------------------- */
-
-    /* Special class on .container surrounding .navbar, used for positioning it into place. */
-    .navbar-wrapper {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      z-index: 10;
-      margin-top: 20px;
-      margin-bottom: -90px; /* Negative margin to pull up carousel. 90px is roughly margins and height of navbar. */
-    }
-    .navbar-wrapper .navbar {
-
-    }
-
-    /* Remove border and change up box shadow for more contrast */
-    .navbar .navbar-inner {
-      border: 0;
-      -webkit-box-shadow: 0 2px 10px rgba(0,0,0,.25);
-         -moz-box-shadow: 0 2px 10px rgba(0,0,0,.25);
-              box-shadow: 0 2px 10px rgba(0,0,0,.25);
-    }
-
-    /* Downsize the brand/project name a bit */
-    .navbar .brand {
-      padding: 14px 20px 16px; /* Increase vertical padding to match navbar links */
-      font-size: 16px;
-      font-weight: bold;
-      text-shadow: 0 -1px 0 rgba(0,0,0,.5);
-    }
-
-    /* Navbar links: increase padding for taller navbar */
-    .navbar .nav > li > a {
-      padding: 15px 20px;
-    }
-
-    /* Offset the responsive button for proper vertical alignment */
-    .navbar .btn-navbar {
-      margin-top: 10px;
-    }
-
-
-
-    /* CUSTOMIZE THE NAVBAR
-    -------------------------------------------------- */
-
-    /* Carousel base class */
-    .carousel {
-      margin-bottom: 60px;
-    }
-
-    .carousel .container {
-      position: relative;
-      z-index: 9;
-    }
-
-    .carousel-control {
-      height: 80px;
-      margin-top: 0;
-      font-size: 120px;
-      text-shadow: 0 1px 1px rgba(0,0,0,.4);
-      background-color: transparent;
-      border: 0;
-    }
-
-    .carousel .item {
-      height: 500px;
-    }
-    .carousel img {
-      position: absolute;
-      top: 0;
-      left: 0;
-      min-width: 100%;
-      height: 500px;
-    }
-
-    .carousel-caption {
-      background-color: transparent;
-      position: static;
-      max-width: 550px;
-      padding: 0 20px;
-      margin-top: 200px;
-    }
-    .carousel-caption h1,
-    .carousel-caption .lead {
-      margin: 0;
-      line-height: 1.25;
-      color: #fff;
-      text-shadow: 0 1px 1px rgba(0,0,0,.4);
-    }
-    .carousel-caption .btn {
-      margin-top: 10px;
-    }
-
-
-
-    /* MARKETING CONTENT
-    -------------------------------------------------- */
-
-    /* Center align the text within the three columns below the carousel */
-    .marketing .span4, .desc {
-      text-align: center;
-    }
-    .marketing h2 {
-      font-weight: normal;
-    }
-    .marketing .span4 p {
-      margin-left: 10px;
-      margin-right: 10px;
-    }
-
-
-    /* Featurettes
-    ------------------------- */
-
-    .featurette-divider {
-      margin: 80px 0; /* Space out the Bootstrap <hr> more */
-    }
-    .featurette {
-      overflow: hidden; /* Vertically center images part 2: clear their floats. */
-    }
-
-    /* Give some space on the sides of the floated elements so text doesn't run right into it. */
-    .featurette-image{
-      width: 400px;
-    }
-    .featurette-image.pull-left {
-      margin-right: 40px;
-    }
-    .featurette-image.pull-right {
-      margin-left: 40px;
-    }
-
-    /* Thin out the marketing headings */
-    .featurette-heading {
-      font-size: 50px;
-      font-weight: 300;
-      line-height: 1;
-      letter-spacing: -1px;
-    }
-
-
-
-    /* RESPONSIVE CSS
-    -------------------------------------------------- */
-
-    @media (max-width: 979px) {
-
-      .container.navbar-wrapper {
-        margin-bottom: 0;
-        width: auto;
-      }
-      .navbar-inner {
-        border-radius: 0;
-        margin: -20px 0;
-      }
-
-      .carousel .item {
-        height: 500px;
-      }
-      .carousel img {
-        width: auto;
-        height: 500px;
-      }
-
-      .featurette {
-        height: auto;
-        padding: 0;
-      }
-      .featurette-image.pull-left,
-      .featurette-image.pull-right {
-        display: block;
-        float: none;
-        max-width: 40%;
-        margin: 0 auto 20px;
-      }
-    }
-
-
-    @media (max-width: 767px) {
-
-      .navbar-inner {
-        margin: -20px;
-      }
-
-      .carousel {
-        margin-left: -20px;
-        margin-right: -20px;
-      }
-      .carousel .container {
-
-      }
-      .carousel .item {
-        height: 300px;
-      }
-      .carousel img {
-        height: 300px;
-      }
-      .carousel-caption {
-        width: 65%;
-        padding: 0 70px;
-        margin-top: 100px;
-      }
-      .carousel-caption h1 {
-        font-size: 30px;
-      }
-      .carousel-caption .lead,
-      .carousel-caption .btn {
-        font-size: 18px;
-      }
-
-      .marketing .span4 + .span4 {
-        margin-top: 40px;
-      }
-
-      .featurette-heading {
-        font-size: 30px;
-      }
-      .featurette .lead {
-        font-size: 18px;
-        line-height: 1.5;
-      }
-
-    }
-    </style>
+    <link href="cr-default.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -299,56 +60,45 @@
 
 
 
-    <!-- Carousel
-    ================================================== -->
-    <div id="myCarousel" class="carousel slide">
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="img/nova_imagem_home7.jpg" alt="Lavagem de sofá">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Sofá</h1>
-              <p class="lead">A higienização dos estofados é realizada no próprio ambiente do cliente. </p>
-              <a class="btn btn-large btn-primary" href="contato.php" onclick="_gaq.push(['_trackEvent', 'Carrosel', 'Home', 'Carrosel 1 - sofa']);">Faça seu orçamento</a>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img src="img/nova_imagem_home8.jpg" alt="Lavagem de colchões">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Estofados</h1>
-              <p class="lead">Nossa equipe de limpeza orienta os clientes como obter uma boa conservação.</p>
-              <a class="btn btn-large btn-primary" href="contato.php" onclick="_gaq.push(['_trackEvent', 'Carrosel', 'Home', 'Carrosel 2 - Colcoes e estofados']);">Faça seu orçamento</a>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img src="img/nova_imagem_home9.jpg" alt="Lavagem de cadeiras">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Cadeiras</h1>
-              <p class="lead">Remoção de ácaros e sujeiras com eficiência.</p>
-              <a class="btn btn-large btn-primary" href="contato.php" onclick="_gaq.push(['_trackEvent', 'Carrosel', 'Home', 'Carrosel 3 - Cadeiras']);">Faça seu orçamento</a>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-    </div><!-- /.carousel -->
-
-
-
     <!-- Marketing messaging and featurettes
     ================================================== -->
     <!-- Wrap the rest of the page in another container to center all the content. -->
 
     <div class="container marketing">
 
+      <hr class="featurette-divider">
+
+      <div id="myCarousel" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="item">
+            <img src="http://placehold.it/1200x400" alt="">
+            <div class="carousel-caption">
+              <h4>First Thumbnail label</h4>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="http://placehold.it/1300x510" alt="">
+            <div class="carousel-caption">
+              <h4>Second Thumbnail label</h4>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+          </div>
+          <div class="item active">
+            <img src="http://placehold.it/1400x520" alt="">
+            <div class="carousel-caption">
+              <h4>Third Thumbnail label</h4>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </div>
+          </div>
+        </div>
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+      </div>
+
+      <hr class="featurette-divider">
       <!-- Three columns of text below the carousel -->
-      <div class="row desc jumbotron">
+      <div class="desc jumbotron">
         <!-- <img src="img/logo.jpg" alt="CR Serviços de limpeza" title="CR Serviços de limpeza"> -->
         <h1 class="featurette-heading"><span class="muted">Lavagem de Sofá</span></h1>
         <p class="lead">
@@ -361,6 +111,7 @@
 Depois de finalizado a <strong>lavagem de sofá a seco</strong>, aconselhamos manter uma boa manutenção preventiva, assim você estará contribuindo para um melhor estilo de vida dos usuários.</p>
         
         <p class="lead"><span class="muted"> Faça seu pedido de orçamento agora:</span></p>
+        
        <form name="contato" action="#" method="post">
 
           <div class="control-group preencher">
@@ -530,16 +281,12 @@ Depois de finalizado a <strong>lavagem de sofá a seco</strong>, aconselhamos ma
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/jquery.js"></script>
-    <script src="js/bootstrap-transition.js"></script>
     <script src="js/bootstrap-collapse.js"></script>
     <script src="js/bootstrap-carousel.js"></script>
     <script>
-      !function ($) {
-        $(function(){
-          $('#myCarousel').carousel()
-        })
-      }(window.jQuery)
-
+      $('.carousel').carousel({
+        interval: 2000
+      })
   	  var _gaq = _gaq || [];
   	  _gaq.push(['_setAccount', 'UA-36923842-1']);
   	  _gaq.push(['_setDomainName', 'crlimpezas.com.br']);
